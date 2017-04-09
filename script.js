560845bb71faf98ef1137cf6de6893e96248635b
MichaelisMods <michaelisex@protonmail.com>
2017-04-09 14:33:25 -0500

    fixed
    some stupid github error

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-fuck githup
+(function() {
+	window.addEventListener("load", function() {
+		var exileHtmlInject = '<div class="barMiddle">' +
+'<style type="text/css">' +
+	'.exilePop' +
+	'{' +
+		'text-align: center;' +
+		'font-weight: bold;' +
+	'}' +
+	'.exileButton' +
+	'{' +
+		'font-weight: bold;' +
+		'color: cyan;' +
+		'font-family: sans-serif;' +
+		'background-color: black;' +
+		'border-radius: 5px;' +
+		'margin-top: 3px;' +
+		'margin-bottom: 3px;' +
+		'margin-left: 3px;' +
+		'margin-right: 3px;' +
+	'}' +
+'</style>' +
+	'<div class="toolbarButton liveButton hidden" data-tooltip="Let anyone on Rabbit join you!"></div>' +
+	'<div class="toolbarButton rabbitcastLaunchpadButton" data-tooltip="Launchpad"><span></span></div>' +
+	'<div class="toolbarButton rabbitcastText hidden">Find Something to Watch</div>' +
+	'<div class="toolbarDivider"></div>' +
+	'<div class="toolbarButton screencastControlButton someoneIsInControll" data-tooltip="Remote Control"></div>' +
+	'<div class="takeScreencastControlRegion"></div>' +
+	'<div class="toolbarButton volumeControl" data-tooltip="Volume">' +
+		'<div class="volumeIcon volume-3"></div>' +
+		'<span><div class="volumeSlider ui-slider ui-slider-horizontal ui-widget ui-wi' +
+		'dget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all' +
+		'ui-slider-range-min" style="width: 100%;"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 100%;"></span></div></span>' +
+	'</div>' +
+	'<div class="toolbarButton screencastQualityButton LD">' +
+		'<div class="tooltipHolder" data-tooltip="Rabbitcast Quality"></div>' +
+		'<div class="screencastQualityMenu"></div>' +
+	'</div>' +
+	'<!--Start Exile DIVS-->' +
+	'<div class="toolbarButton nigger">' +
+	'<button id = "exileOP" class="exileButton">Chat PopOut</button>' +
+	'</div>' +
+	'<!--End Exil DIVS-->' +
+	'<div class="toolbarButton screencastStopButton notVisible" data-tooltip="Stop Sharing"></div>' +
+	'<div class="toolbarButton gameStopButton notVisible" data-tooltip="Stop Game"></div>' +
+'</div>'
+
+		var middleLoop = setInterval(waitForDiv,500);
+
+		function waitForDiv()
+		{
+			var elem = document.getElementsByClassName("barMiddle")[0];
+			if (typeof elem !== 'undefined')
+			{
+				elem.innerHTML = exileHtmlInject;
+			  document.getElementById('exileOP').onclick = doChatPOP;
+				console.log("GETS !!!");
+				clearInterval(middleLoop);
+			}
+			else
+			{
+				console.log("Missed :c");
+			}
+		}
+
+		function doChatPOP() {
+			var w = window.open();
+			w.document.write( document.getElementsByClassName("socialLayout")[0]);
+			w.document.close();
+			alert("Fucko");
+		}
+
+	});
+})();
